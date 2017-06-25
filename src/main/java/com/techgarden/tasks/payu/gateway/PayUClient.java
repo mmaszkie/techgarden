4d70f99839adb3b5855d7a19b103e73732da59bc
Mateusz Maszkiewicz <mmaszkie@gmail.com>
2017-06-26 00:19:59 +0200

    Remove unnecessary annotation

--- src/main/java/com/techgarden/tasks/payu/gateway/PayUClient.java
+++ src/main/java/com/techgarden/tasks/payu/gateway/PayUClient.java
@@ -8,13 +8,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-@EnableAutoConfiguration
 class PayUClient {
 
     private static final Logger logger = LoggerFactory.getLogger(PayUClient.class);
